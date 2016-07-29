aa914b4da7e93156b960ccd277248dbf08417da3
andystanier <andystanier@users.noreply.github.com>
2016-07-29 17:34:28 +0100

    did the skills page

--- js/main.js
+++ js/main.js
@@ -1,13 +1,6 @@
-/*
-$(document).ready(function(){
-	$(".name").click(function(){
-		$(this).css("color", "red");
-	});
-});
-*/
-
 $(document).ready(function(){
 	
+	// to change which tab you're looking at
 	$('ul.tabs li').click(function(){
 		var tab_id = $(this).attr('data-tab');
 
@@ -18,12 +11,23 @@ $(document).ready(function(){
 		$("#"+tab_id).addClass('current');
 	})
 
-	$('a#education-link').click(function(){
+	// When there's an internal link, go to that tab
+	$('.intLink').click(function(){
+		var fromTab = $('.tab-content.current').attr('id');
+		var toTab = $(this).attr('data-tab');
+		
+		$('#'+fromTab).removeClass('current');
+		$('#'+fromTab+'Tab').removeClass('current');
 
-		$('#work').removeClass('current');
-		$('#workTab').removeClass('current');
-
-		$("#education").addClass('current');
-		$("#educationTab").addClass('current');
-	})
+		$('#'+toTab).addClass('current');
+		$('#'+toTab+'Tab').addClass('current');
+	});
+	
+	// shows some info about the think you're mousing over
+	$('td').mouseenter(function(){
+		var tableId = $(this).attr('id');  //java
+		var textLabel = tableId+"Text"; //javaText
+		var html = $('#'+textLabel).html();
+		$('.explainText').html(html);
+	});
 })
