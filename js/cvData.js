fb08566e229dad518da1d973690b5e44baa61771
andystanier <andystanier@users.noreply.github.com>
2016-09-07 17:24:33 +0100

    finished Array Website

--- js/cvData.js
+++ js/cvData.js
@@ -1,103 +1,93 @@
 var cvData = {
 		// Personal Details
-		"personalStatement":"<p>Andy Stanier has recently completed a masters degree in computer science.  "+
-							"Originally with a background in chemistry, and with 15 years experience in the "+
-							"pharmaceutical industry, [he is a intermediate level programmer, eager to learn new "+
-							"techniques and languages]</p>"+
-							"<p>Having being made redundant from the pharmaceutical industry several times, he decided "+
-							"to pursue his long-held interests in computer programming.  Having undertaken several "+
-							"different online courses in programming and computer science and using books and YouTube videos "+
-							"to learn new skills, he enrolled in a masters degree in computer science with the University of "+
+		"personalStatement":"<p>Andy Stanier has recently completed a master's degree in computer science.  "+
+							"With a background in chemistry and 15 years' experience in the pharmaceutical industry, "+
+							"Andy is an intermediate level programmer in common languages and is "+
+							"eager to learn new techniques and languages as required.</p>"+
+							"<p>Having being made redundant from the pharmaceutical industry several times, Andy decided "+
+							"to pursue his long-held interest in computer programming.  Having undertaken several "+
+							"different online courses in programming and computer science as well as using books and YouTube videos "+
+							"to learn new skills, he enrolled in a computer science master's degree at the University of "+
 							"Hertfordshire, thus enabling him to gain a recognised formal qualification.</p>",
 
 		// Skills
 		"skillsText":"Andy has learned various different languages, techniques and software packages over the years, especially "+
-					"whilst studying for his Masters degree.  Some of these were learned prior to his formal study however the +"+
+					"whilst studying for his master's degree.  Some of these were learned prior to his formal study however the "+
 					"majority were learned during the last year. ",
 
-		"transferSkills":"<p>Andy has also gained many transferable skills during 15 years working in the pharmaceutical industry</p>"+
+		"transferSkills":"<p>Andy has also gained many transferable skills during 15 years working in the pharmaceutical industry:</p>"+
 						"<ul>"+
-							"<li>Problem Solving - overcoming day-to-day issues with synthesis</li>"+
-							"<li>Research Skills - consulting the literature for ideas and solutions</li>"+
-							"<li>Planning - designing the best synthetic route to a set of compounds</li>"+
-							"<li>Tenacity - working to produce compounds despite setbacks</li>"+
-							"<li>Working to deadlines - completing synthesis by a required testing date</li>"+
-							"<li>Teamwork - working in project teams of between 4 and 14 people</li>"+
-							"<li>Independence - working unaccompanied on sub-projects</li>"+
+							"<li>Problem Solving - overcoming day-to-day issues with chemical synthesis.</li>"+
+							"<li>Research Skills - consulting the literature for ideas and solutions.</li>"+
+							"<li>Planning - designing the best synthetic route to a set of compounds.</li>"+
+							"<li>Tenacity - working to produce compounds despite setbacks.</li>"+
+							"<li>Working to deadlines - completing synthesis by a required testing date.</li>"+
+							"<li>Teamwork - working in project teams of between 4 and 14 people.</li>"+
+							"<li>Independence - working unaccompanied on sub-projects.</li>"+
 						"</ul>",
 
 		// Work History
-		"molplexExpBody":"This job ended due to redundancy.  "+
-						"Initially helping to set up a functional lab and office environment. Implementing working practices.  "+
-						"Identifying and obtaining lab equipment and chemistry software tools.  "+
-						"Main responsibilities were compound planning, design, synthesis, identification and delivery.  "+
-						"Safety officer for the lab; including writing SOPs, safety policies, fire sweeper.  "+
-						"Auditing of lab equipment and organising removal of unwanted kit.  ",
+		"molplexExpBody":"This job ended in redundancy due to closure of the chemistry department.  "+
+						"Molplex was a small biochemical start-up, whose aim was to use "+
+						"Artificial Intelligence to design new drugs for orphan diseases.  Roles included: "+
+						"Establishing a working chemistry lab and office environment.  "+
+						"Identifying and obtaining laboratory equipment and chemistry software tools at a reasonable price.  "+
+						"Route design, synthesis and analysis of new chemical compounds for biochemical screening.  "+
+						"Lab Safety officer including writing SOPs and safety policies.  ",
 
-		"betweenChemJobsExpBody":"During this period, attended Jobcentre run courses in CV writing, IT courses and applied "+
+		"betweenChemJobsExpBody":"During this period, Andy attended Jobcentre run courses in CV writing, went to IT courses and applied "+
 								"for jobs in the pharmaceutical industry.  ",
 
 		"eisaiExpBody":"This job ended in redundancy due to closure of the chemistry department.  "+
-						"Designed synthetic strategies that provided scaffolds for CNS focused libraries which could easily "+
-						"be functionalised to final compounds for ADME screening.  "+
-						"Improved synthetic route to tool compounds to support project assays with batches for in-vivo "+
-						"testing successfully delivered. Recognised within the chemistry group for problem solving and "+
-						"offering advice. Prepared and delivered presentations to communicate progress and problems to "+
-						"colleagues at project and departmental meetings. Supported the day-to-day maintenance of analytical "+
-						"and preparative HPLC using expertise gained at previous employment.  ",
+						"Designed and synthesised libraries of compounds for screening.  "+
+						"Prepared and delivered presentations to communicate progress and problems to "+
+						"colleagues at project and departmental meetings.",
 
 		"charnwoodExpBody":"This job ended in redundancy due to closure of the chemistry department.  "+
-						"Designed and synthesised novel compounds within restricted time frames for biological screening against "+
-						"respiratory targets.  Involved with seeking out and testing new laboratory "+
-						"equipment to increase departmental productivity.  Permission was secured to purchase new equipment.  "+
-						"Coached sandwich student, advising on practical technique, problem solving and questioning on reaction mechanism.  "+
-						"Developed expertise in Unipoint and Trilution software for use in preparative HPLC.  ",
+						"Designed and synthesised novel compounds within for biological screening against respiratory targets.",
 
 		"alderleyExpBody":"This job ended in redundancy due to closure of the research area.  "+
-						"Worked within various project teams to deliver compounds for biological screening in Lead Generation "+
-						"in Oncology and Inflammation areas, with some projects progressing to lead optimisation stages.  "+
-						"Appointed route design expert in a High Throughput Chemistry and Purification Group.  Invented "+
-						"synthetic schemes that were amenable to high capacity synthesis and purification, resulting in small "+
-						"libraries of compounds for SAR purposes. Laboratory Safety Co-ordinator and Permit to Work issuer for "+
-						"local area.  ",
+						"Worked within project teams to deliver compounds for biological screening in early and late stage discovery.",
 
 		// Education
 		"uohEd":"<h4>Computer Science at University of Hertfordshire, UK</h4>"+
-						"<p>A distance learning Masters degree in Computer Science, the following courses were studied:</p>"+
+						"<p>A distance learning Master's degree in Computer Science, studying the following:</p>"+
 						"<ul>"+
-							"<li><span class='courseNames'>Programming and Program Design</span><br/>Learning "+
-							"<span class='strong'>Java</span> language and Object Oriented Programming techniques using the "+
+							"<li><span class='courseNames'>Programming and Program Design</span><br/>"+
+							"Learning <span class='strong'>Java</span> language and Object Oriented Programming techniques using the "+
 							"<span class='strong'>BlueJ</span> IDE.  Assessment by exams and coursework; the final "+
-							"piece being a program, written from scratch, to a provided specification.</li>"+
+							"piece was a program, written from scratch, to a provided specification.</li>"+
 
-							"<li><span class='courseNames'>Web Scripting and Application Development</span><br/>Learning about server-side "+
-							"website design using <span class='strong'>ASP.NET</span>, <span class='strong'>C#</span> and "+
-							"<span class='strong'>SQL</span>.  Created a website of user generated content.</li>"+
+							"<li><span class='courseNames'>Web Scripting and Application Development</span><br/>"+
+							"Learning about server-side website design using <span class='strong'>ASP.NET</span>, "+
+							"<span class='strong'>C#</span> and <span class='strong'>SQL</span>.  "+
+							"Created a website of user generated content.</li>"+
 
-							"<li><span class='courseNames'>Contemporary Practices in Information Technology</span><br/>The ethics of "+
-							"information technology assessed by research and writing essays.</li>"+
+							"<li><span class='courseNames'>Contemporary Practices in Information Technology</span><br/>"+
+							"The ethics of information technology, assessed by researching and writing essays.</li>"+
 
-							"<li><span class='courseNames'>Mobile Standards, Interfaces and Applications</span><br/>Learning about "+
-							"Ubiquitous computing, HCI, Wearable Computing, Context Awareness and using "+
+							"<li><span class='courseNames'>Mobile Standards, Interfaces and Applications</span><br/>"+
+							"Learning about Ubiquitous computing, HCI, Wearable Computing, Context Awareness and using "+
 							"<span class='strong'>JavaScript</span>, <span class='strong'>JSON</span> and "+
 							"<span class='strong'>jQuery Mobile</span> to generate a mobile website.</li>"+
 
-							"<li><span class='courseNames'>Secure Systems Programming</span><br/>Exploring security problems with the "+
-							"<span class='strong'>C</span> programming language and how to avoid them and learning about DDOS, BotNets, "+
-							"Worms and Viruses.</li>"+
+							"<li><span class='courseNames'>Secure Systems Programming</span><br/>"+
+							"Exploring security problems with the <span class='strong'>C</span> programming language and "+
+							"how to avoid them, plus learning about DDOS, BotNets, Worms and Viruses.</li>"+
 
-							"<li><span class='courseNames'>Software Development Tools and Methods</span><br/>Ways to develop software, "+
-							"<span class='strong'>UML</span>, <span class='strong'>XML</span>, secure systems development.  Assessment "+
-							"by essay and coursework drawing UML diagrams.</li>"+
+							"<li><span class='courseNames'>Software Development Tools and Methods</span><br/>"+
+							"Ways to develop software, <span class='strong'>UML</span>, <span class='strong'>XML</span>, "+
+							"secure systems development.  Assessment by essay and coursework drawing UML diagrams.</li>"+
 
-							"<li><span class='courseNames'>Final Project in Computer Science</span><br/>A piece of research and practical "+
-							"work about Electronic Voting Systems, creating a desktop <span class='strong'>Java</span> application and an "+
-							"<span class='strong'>HTML5</span> website for vote verification.  Verson control was handled by "+
-							"<span class='strong'>Git</span>.</li>"+
+							"<li><span class='courseNames'>Final Project in Computer Science</span><br/>"+
+							"A piece of research and practical work about Electronic Voting Systems, creating a desktop "+
+							"<span class='strong'>Java</span> application and an <span class='strong'>HTML5</span> website "+
+							"for vote verification.  Verson control was handled by <span class='strong'>Git</span> software.</li>"+
 						"</ul>"+
 						"<p>As well as these taught languages and skills, <span class='strong'>C++</span>, "+
-						"<span class='strong'>TypeScript</span>, <span class='strong'>Node.js</span>, <span class='strong'>Angular.js</span> "+
-						"and <span class='strong'>AJAX</span> were investigated during private study.</p>",
+						"<span class='strong'>TypeScript</span>, <span class='strong'>Node.js</span>, "+
+						"<span class='strong'>Angular.js</span> and <span class='strong'>AJAX</span> "+
+						"were investigated during private study.</p>",
 
 		"uoyEd":"<h4>Chemistry at University of York, UK</h4>"+
 				"<p>Chemistry with a Year in Industry, MChem with First Class Honours.<br />"+
@@ -110,15 +100,15 @@ var cvData = {
 				"<p>Four 'A' Levels in Mathematics (A), Chemistry (A), Physics (B) and General Studies (A).</p>",
 
 		// Projects
-		"oscarProj":" - This is a website written as a piece of coursework for a mobile applications course and demonstrates "+
+		"oscarProj":" - This is a website written as a piece of coursework for a mobile applications course which demonstrates "+
 					"the use of JQuery mobile.  It contains a summary of all the films that have won 'Best Picture' Oscar since "+
 					"the first Academy Awards in 1928 to present day, and allows users to tick them off when they have seen them.  ",
 
-		"musicProj":" - This was a website written to practice using jQuery mobile in a simple website.  This project is incomplete "+
-					"however it demonstrates the use of jQuery mobile page structure and functions.",
+		"musicProj":" - This was a website written to practice using jQuery mobile in a simple website.  This project is partially "+
+					"complete, however it demonstrates the use of jQuery mobile page structure and functions.",
 
 		"rodentProj":" - This was an end of course project to show the use of ASP.NET and C# for a user generated content website, "+
-					"using SQL database and server side user interface.  ",
+					"using an SQL database and server side user interface.  ",
 
 		"evoteProj":" - This was developed as part of the final project. It is a desktop Java application which simulates the creation "+
 					"and running of an election with electronic voting.  The desktop program connects to an SQL database to collate votes.  ",
@@ -127,5 +117,5 @@ var cvData = {
 					"The website uses ASP.NET, C# and SQL to create and control the content.  ",
 
 		"yahtzeeProj":" - A Python 3 program written to simulate the rolling of 5 dice simultaneously until five-of-a-kind is rolled, "+
-					"the process is repeated many times and the max, min and average number of rolls needed is recorded.  "
+					"the process is repeated many times and the maximum, minimum and average number of rolls needed is recorded.  "
 };
